79db50ae2e2603418728bbe8c80a32c98b96a9f0
zhekas <zhekas2003@mail.ru>
2024-05-21 10:14:31 +0300

    l[plp;oii9k

--- Weather.cs
+++ Weather.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -10,16 +11,23 @@ namespace Прогноз_погоды
 {
     internal class Weather
     {
+        public static string temp;
+        public static string wind;
+        public static string weather;
+        public static string pressure;
         static string city = "Moscow";
         const string API = "362ba17051da00e13a5a17361174bef3";
         static HttpClient client = new HttpClient();
-        static string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric";
+        static string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric&lang=ru";
         static public async void getWeather()
         {
             string response = await client.GetStringAsync(url);
             string result = response;
             var json = JObject.Parse(result);
-            string temp = json["main"]["temp"].ToString();
+            temp = json["main"]["temp"].ToString();
+            pressure = json["main"]["pressure"].ToString();
+            wind = json["wind"]["speed"].ToString();
+            weather = json["weather"][0]["description"].ToString();
         }
     }
 } 
